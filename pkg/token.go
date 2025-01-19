6d12d4e2b2084c92131d7bcaf8edc5cf17c21dd6
NDIV123 <dmarshelano@gmail.com>
2025-01-19 12:09:57 +0700

    feat : create token handling function

--- pkg/token.go
+++ pkg/token.go
@@ -1,8 +1,11 @@
 package pkg
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/spf13/viper"
 )
@@ -23,4 +26,65 @@ func GenerateToken(email, id string) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func TokenValid(c *gin.Context) error {
+	tokenString, err := ExtractToken(c)
+	if err != nil {
+		return err
+	}
+
+	_, err = jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method %v", t.Header["alg"])
+		}
+
+		return []byte(viper.GetString("SECRET")), nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExtractToken(c *gin.Context) (string, error) {
+	token, err := c.Cookie("access_token")
+	if err != nil {
+		return "", err
+	}
+	if token != "" {
+		return token, nil
+	}
+
+	bearerToken := c.Request.Header.Get("Authorization")
+
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1], nil
+	}
+
+	return "", nil
+}
+
+func ExtractTokenId(c *gin.Context) (any, error) {
+	tokenString, err := ExtractToken(c)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
+		}
+		return []byte(viper.GetString("SECRET")), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok && !token.Valid {
+		return 0, err
+	}
+
+	return claims["subject"], nil
 }
