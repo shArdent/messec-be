82755f90f8d5689bcdc74b1a44e17fd08c65d00b
NDIV123 <dmarshelano@gmail.com>
2025-01-21 16:10:42 +0700

    feat (comment) : create post comment controller

--- internal/comment/comment.controller.go
+++ internal/comment/comment.controller.go
@@ -2,11 +2,13 @@ package comment
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func PostComment(c *gin.Context) {
+	postId := c.Param("post_id")
 	var newComment Comment
 
 	if err := c.ShouldBindJSON(&newComment); err != nil {
@@ -14,6 +16,26 @@ func PostComment(c *gin.Context) {
 			"error":  "Invalid request data",
 			"detail": err.Error(),
 		})
+		return
+	}
+
+	value, err := strconv.ParseUint(postId, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Invalid request data",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	newComment.PostID = uint(value)
+
+	if err := CreateComment(&newComment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Invalid request data",
+			"detail": err.Error(),
+		})
+		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{
