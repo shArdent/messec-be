db79ce08ae9f2f9efa597f6a5f143df91765fa00
NDIV123 <dmarshelano@gmail.com>
2025-01-22 06:11:49 +0700

    feat(question) : create post comment repo

---
+++ internal/question/question.repository.go
@@ -0,0 +1,15 @@
+package question
+
+import (
+	"github.com/shardent/messec-be/infra/database"
+	"github.com/shardent/messec-be/infra/logger"
+)
+
+func Create(model interface{}) error {
+	err := database.DB.Create(&model).Error
+	if err != nil {
+		logger.Errorf("Error creating question", err.Error())
+	}
+
+	return err
+}
