b47d3fc3e5d0eb7c6ea8bd617fcad5df6e5fd766
NDIV123 <dmarshelano@gmail.com>
2025-01-21 13:35:38 +0700

    feat (post controller) : create getpostsbyid function

--- internal/post/post.controller.go
+++ internal/post/post.controller.go
@@ -1,14 +1,26 @@
 package post
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/shardent/messec-be/infra/database"
 )
 
 func GetAllPostByUserId(c *gin.Context) {
-    userId := c.Param("user_id")
-    var posts *[]Post
+	userId := c.Param("user_id")
 
-    err := GetPostsByUserId(&posts[], userId)
+	posts, err := GetPostsByUserId(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Error getting posts data",
+			"detail": err.Error(),
+		})
+		return
+	}
 
+    c.JOSN(http.StatusOk, gin.H{
+        "message" : "Success retrieve post data",
+        "posts" : posts,
+    })
 }
