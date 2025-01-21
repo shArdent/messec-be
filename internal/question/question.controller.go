926faa2662e3c41f3ab595c71cf705fc02f0ff33
NDIV123 <dmarshelano@gmail.com>
2025-01-22 06:12:21 +0700

    feat(question) : create post comment controller function

---
+++ internal/question/question.controller.go
@@ -0,0 +1,43 @@
+package question
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateComment(c *gin.Context) {
+	userID := c.Param("user_id")
+	var newQuestion Question
+
+	if err := c.ShouldBindJSON(&newQuestion); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Error Bad Request",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	convUserID, err := strconv.ParseUint(userID, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Invalid UserID",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	newQuestion.UserID = uint(convUserID)
+	if err := Create(&newQuestion); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":  "Error creating question",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "comment created",
+	})
+}
