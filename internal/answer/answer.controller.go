ba5a08b00d0dd541fc9dfba8e3059dfef19ba3e6
NDIV123 <dmarshelano@gmail.com>
2025-01-22 06:58:53 +0700

    feat : create post answer

---
+++ internal/answer/answer.controller.go
@@ -0,0 +1,56 @@
+package answer
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shardent/messec-be/pkg"
+)
+
+func PostAnswer(c *gin.Context) {
+	questionId := c.Param("post_id")
+
+	var newAnswer Answer
+
+	if err := c.ShouldBindJSON(&newAnswer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Invalid request data",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	value, err := strconv.ParseUint(questionId, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Invalid request data",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	userId, err := pkg.ExtractTokenId(c)
+	if err == nil {
+		conv64UserId, _ := strconv.ParseUint(userId.(string), 10, 64)
+
+		convUintUserId := uint(conv64UserId)
+
+		newAnswer.UserID = convUintUserId
+
+	}
+
+	newAnswer.QuestionID = uint(value)
+
+	if err := CreateComment(&newAnswer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "Invalid request data",
+			"detail": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Answer Posted posted",
+	})
+}
--- internal/question/question.route.go
+++ internal/question/question.route.go
@@ -4,6 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func SetupRoutes(g *gin.RouterGroup) {
 	question := g.Group("/questions")
-	question.GET("/user_id", GetAllQuestionByUserId)
+    question.GET("/:user_id", GetAllQuestionByUserId)
 	question.POST("/:user_id", CreateQuestion)
 }
