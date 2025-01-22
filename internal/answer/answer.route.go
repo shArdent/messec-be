9a4aad26512cc9e99e8fd7cf0c9796a8c40c0443
NDIV123 <dmarshelano@gmail.com>
2025-01-22 19:58:54 +0700

    feat : add jwt middleware to answer POST

--- internal/answer/answer.route.go
+++ internal/answer/answer.route.go
@@ -1,8 +1,11 @@
 package answer
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shardent/messec-be/infra/middlewares"
+)
 
 func SetupRoutes(g *gin.RouterGroup) {
 	answer := g.Group("/answers")
-	answer.POST("/:question_id", PostAnswer)
+	answer.POST("/:question_id", middlewares.JwtAuthMiddleware(), PostAnswer)
 }
