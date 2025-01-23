dc3891ee380bd25b9417ee23e11b0cdd197faf76
NDIV123 <dmarshelano@gmail.com>
2025-01-23 09:44:43 +0700

    feat : add delete controller to router

--- internal/comment/comment.route.go
+++ internal/comment/comment.route.go
@@ -1,9 +1,13 @@
 package comment
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shardent/messec-be/infra/middlewares"
+)
 
 func SetupRoutes(g *gin.RouterGroup) {
 	comment := g.Group("/comments")
-    // create comment by postId  
+	// create comment by postId
 	comment.POST("/:post_id", PostComment)
+	comment.DELETE("/:comment_id", middlewares.JwtAuthMiddleware(), DeleteComment)
 }
